7a5b1201e1de299798727cb419b9f46f24fabe5c
Hanin Nouh <haninnouh@gmail.com>
2020-01-14 14:27:42 +0300

    attraction-resturant

--- src/containers/Attraction.js
+++ src/containers/Attraction.js
@@ -12,13 +12,15 @@ import {
         super(props);
         this.state={
             landmark: [], 
-            // name:[],
-            // website:[],
-            // img:[],
+         name:[],
+        website:[],
+         img:[],
           cityIdR: this.props.cityIdAttraction
         }
        // console.log(this.props.cityIdAttraction+"this.props.cityIdAttraction")
     }
+
+    
 componentDidMount(){
 const axios = require("axios");
 
@@ -38,56 +40,102 @@ axios({
     "bookable_first":"false",
     "subcategory":"36",
     "location_id":"186338"
-    // `${this.props.cityIdAttraction}`
+        // `${this.props.cityIdAttraction}`
+
     }
     })
     .then((response)=>{
         console.log("i am in then")
-      console.log(response)
-      
-      for(var i=0;i<response.data.data.length;i++){
-          console.log("I am in for")
-          this.setState(prevStat=>({
-              landmark:prevStat.landmark.push({
-                  name: response.data.data[i].name,
-                  website: response.data.data[i].website,
-                  img: response.data.data[i].photo.images.small.url
-              })
-          }))
-                
-        // this.setState({name: this.state.name.concat(response.data.data[i].name)})
-        // this.setState({website: this.state.website.concat(response.data.data[i].website)})
-        // this.setState({img: this.state.img.concat(response.data.data[i].photo.images.small.url)} )
-    }
+        console.log(response)
+      for(let i in response.data.data){
+          console.log("i am in for")
+           this.setState({name: this.state.name.concat(response.data.data[i].name)})
+            this.setState({website: this.state.website.concat(response.data.data[i].website)})
+           this.setState({img: this.state.img.concat(response.data.data[i].photo.images.small.url)} )
+    
+        //  this.setState(prev=>{
+        //     prev.landmark.concat({
+        //          name: response.data.data[0].name,
+        //         //  website: response.data.data[i].website,
+        //        // img: response.data.data[i].photo.images.small.url
+        //     }
+        //     )
+        //  })  
+      }
+
+      //samar
+    //   for(var i=0;i<response.data.data.length;i++){
+    //       console.log("I am in for")
+    //       this.setState(prevStat=>({
+    //           landmark:prevStat.landmark.push({
+    //               name: response.data.data[i].name,
+    //               website: response.data.data[i].website,
+    //               img: response.data.data[i].photo.images.small.url
+    //           })
+    //       }))
+    //}    
+
+    //abdullah
+    //     // this.setState({name: this.state.name.concat(response.data.data[i].name)})
+    //     // this.setState({website: this.state.website.concat(response.data.data[i].website)})
+    //     // this.setState({img: this.state.img.concat(response.data.data[i].photo.images.small.url)} )
+    // }
+
     //   this.setState({ 
     //     name:response.data.data[0].name,
     //     website:response.data.data[0].website,
     //     img:response.data.data[0].photo.images.small.url  
     //   });
 
-    // console.log(response.data.data.name)
-    // console.log(response.data.data.website)
-    // console.log(response.data.data.photo.images.small)
-
     })
     .catch((error)=>{
       console.log(error)
     })
+    
 }
+
 render(){
-    //console.log(this.state.landmark)
+    
     return(
         <div>
             <div>
-                
-                <h3>{this.state.landmark[0].name}</h3>
-                {/* <p>{this.state.website}</p>
-                <img src={this.state.img}  alt="..." /> */}
-            
+               {this.DisplayAttrac()} 
+                {/* <h3>{this.state.landmark[0].name}</h3> */}
+                 {/* <p>{this.state.name}</p>
+                <p>{this.state.website}</p>
+            <img src={this.state.img}  alt="..." /> */}
+                <br></br>
+                <br></br>
+                <br></br>
             </div>
         </div> 
     );
 
+}
+DisplayAttrac(){
+    // if(this.state.name[index]=="undefined"||this.state.website[index]=="undefined"){
+    //     continue
+    // }
+    // else{
+        return(
+            <div>
+            
+                { this.state.name.map((n, index) => (
+                    
+                    <div>
+                    <h3>{this.state.name[index]}</h3>
+                    <h3>{this.state.website[index]}</h3>
+                    <img src={this.state.img[index]}  alt="..." />
+                    </div>
+            ))}
+            
+            {/* <h3>{this.state.name[i]}</h3>
+            <p>{this.state.website[i]}</p> */}
+            </div>)
+   // }
+    
+        
+   // }
 }
 }
 
--- src/containers/Resturant.js
+++ src/containers/Resturant.js
@@ -11,9 +11,10 @@ import {
     constructor(props){
         super(props);
         this.state={ 
-          name:"",
-          description:"",
-          img:"",
+          name:[],
+          description:[],
+          img:[],
+          rate:[],
           cityIdR: this.props.cityIdResturant
         }
         console.log(this.props.cityIdResturant+"this.props.cityIdResturant")
@@ -37,19 +38,30 @@ axios({
     "restaurant_mealtype":"10598%2C10599",
     "currency":"USD",
     "lang":"en_US",
-    "location_id":`${this.props.cityIdResturant}`
+    "location_id":"186338"
+    // `${this.props.cityIdResturant}`
     }
     })
     .then((response)=>{
       console.log(response)
-      console.log(response.data.data[0].name)
-      console.log(response.data.data[0].description)
-      console.log(response.data.data[0].photo.images.small)
-      this.setState({ 
-        name:response.data.data[0].name,
-        description:response.data.data[0].description,
-        img:response.data.data[0].photo.images.small.url  
-      });
+      // console.log(response.data.data[0].name)
+      // console.log(response.data.data[0].description)
+      // console.log(response.data.data[0].photo.images.small)
+      // this.setState({ 
+      //   name:response.data.data[0].name,
+      //   description:response.data.data[0].description,
+      //   img:response.data.data[0].photo.images.small.url  
+      // });
+      console.log("i am in then")
+    for(let i in response.data.data){
+        console.log("i am in for")
+         this.setState({name: this.state.name.concat(response.data.data[i].name)})
+         this.setState({rate: this.state.rate.concat(response.data.data[i].rating)} )
+          this.setState({description: this.state.description.concat(response.data.data[i].description)})
+         this.setState({img: this.state.img.concat(response.data.data[i].photo.images.small.url)} )
+   
+    }
+
 
     })
     .catch((error)=>{
@@ -59,13 +71,39 @@ axios({
 render(){
     return(
         <div>
-            <h3>{this.state.name}</h3>
+          {this.DisplayRest()} 
+            {/* <h3>{this.state.name}</h3>
             <p>{this.state.description}</p>
-            <img src={this.state.img}  alt="..." />
-
+            <img src={this.state.img}  alt="..." /> */}
+          <br></br>
+          <br></br>
         </div> 
     );
 
+}
+DisplayRest(){
+  // if(this.state.name[index]=="undefined"||this.state.website[index]=="undefined"){
+  //     continue
+  // }
+  // else{
+      return(
+          <div>
+          
+              { this.state.name.map((n, index) => (
+                  
+                  <div>
+                  <h3>{this.state.name[index]}</h3>
+                  <p>{this.state.rate[index]}</p>
+                  <h3>{this.state.description[index]}</h3>
+                  <img src={this.state.img[index]}  alt="..." />
+                  </div>
+          ))}
+          
+          </div>)
+ // }
+  
+      
+ // }
 }
 }
 
