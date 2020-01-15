48c07267842d8a9685067c7a449bc216fc8ddf54
KOSaber <k.o.saber@hotmail.com>
2020-01-15 10:32:35 +0300

    city style

--- src/containers/City.js
+++ src/containers/City.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import Time from './Time';
 import axios from 'axios';
@@ -8,9 +10,7 @@ import {
     Route,
     Link
   } from 'react-router-dom';
-  //const citiesId=["1","2"]
-
-
+  
 export default class City extends React.Component {
     constructor(props){
         super(props)
@@ -19,13 +19,11 @@ console.log(this.props.match.params.id)
     alpha: this.props.alphaCode,
     cities: [],
     citiesId:[],
-    cityName:""
+    citiesName:[]
     }
 }
-
   componentDidMount() {
     // const axios = require("axios");
-
     axios({
       "method": "GET",
       "url": "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
@@ -39,52 +37,46 @@ console.log(this.props.match.params.id)
       // `${this.props.alphaCode}`
    }
     })
-
       .then(res => {
-
-     //   console.log(res)
-     //   console.log(" city res ")
-      //  console.log(this.props.alphaCode+" this.props.alphaCode")
-
+        for(let i=0;i<5;i++){
+            this.setState({cities: this.state.cities.concat(res.data.data[i]) })
+            this.setState({citiesId: this.state.citiesId.concat(res.data.data[i].id) })
+            this.setState({citiesName: this.state.citiesName.concat(res.data.data[i].name) })
+          };
 console.log(res)
-
-      const cities = res.data.data;
-    const citiesId = res.data.data[0].id;
-     const cityName = res.data.data[0].name;
-    this.setState({ cities }); 
-    this.setState({ citiesId });
-    this.setState({cityName});
+   
 })
-
-      
-
       .catch((error) => {
         console.log(error)
       })
-
-
-   
 }
-
   render() {
-    console.log()
-
+    
     return (
-    <Router>
-    <div>
-    {/* <Link to="/Time">Time</Link>{" "} */}
-    <Link to="/GetCityInfo">GetCityInfo</Link>{" "}
-    <Link to="/Time">Time</Link>
-            <ul>
-            { this.state.cities.map(city => <li>{city.name}</li>)}
-            {/* { this.state.citiesId.map(cityId => <li>{cityId.id}</li>)} */}
-            <Route path="/GetCityInfo"  component={() => <GetCityInfo cityName2={this.state.cityName}/> }/>
-            <Route path="/Time"  component={() => <Time cityIdTime={this.state.citiesId}/> }/>
+            
+         <Router> 
+           <div className="row row-cols-3 row-cols-md-2">
+              {/* <Link to="/Time">Time</Link>{" "} */}
+           {/* <Link to="/GetCityInfo">GetCityInfo</Link>{" "} */}
+           {/* <Link to="/Time">Time</Link> */}
+           {this.state.cities.map(city => 
+             <div className="col mb-4">
+              <div className="card">
+                <Link to={"/GetCityInfo/" +  city.name} >
+                   <img src="https://images.unsplash.com/photo-1454942901704-3c44c11b2ad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80 " 
+                   className="card-img-top flagSize" alt="..." /></Link> 
+                 <div className="card-body">
+                   <h3 className="card-title">{city.name}</h3>
+                 </div>
+                 {/* <Route path="/GetCityInfo"  component={() => <GetCityInfo cityName2={this.state.citiesName}/> }/>
+                 <Route path="/Time"  component={() => <Time cityIdTime={this.state.citiesId}/> }/> */}
+               </div> 
+               </div> 
+           )}
 
-        </ul>
-        </div>
-        </Router>
-    )
-  
-}
-}
+          </div>
+      </Router>
+        
+      )
+ }
+ }
